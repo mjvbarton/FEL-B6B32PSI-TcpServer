e5d6f1b2dc4f5ffa4cd5c97cf8724fa6ecbe222b
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-10-24 16:58:57 +0200

    Edited syntax of FOTO.

--- src/main/java/cz/cvut/fel/psi/tcpserver/RequestType.java
+++ src/main/java/cz/cvut/fel/psi/tcpserver/RequestType.java
@@ -13,7 +13,7 @@ public enum RequestType {
     USERNAME("(^Robot)(\\s*\\w+)"),
     PASSWORD("(^\\d+$)"),
     INFO("(^INFO)\\s([^\\r\\n])*"),
-    PHOTO("^FOTO\\s\\d+\\s.+$");
+    PHOTO("^FOTO\\s\\d+\\s[^\\s]+$");
     
     private final Pattern syntax;
         
